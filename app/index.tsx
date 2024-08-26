9dc6571491f0bbfef5beaa6f33a118ec14fdd316
RealTinyWarrior <tahmidradid@gmail.com>
2024-08-26 15:37:46 +0600

    Added mainbox

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { Image, StyleSheet, Text, TouchableNativeFeedback, View } from "react-na
 import { StatusBar } from "expo-status-bar";
 import { LinearGradient } from "expo-linear-gradient";
 import Menus from "../assets/icons/menus";
+import QuranSVG from "../assets/icons/quran";
 
 export default function Home() {
     return (
@@ -10,13 +11,17 @@ export default function Home() {
             <LinearGradient
                 colors={["#9E7CD7", "#ADD8E6", "#F0F8FF", "white"]}
                 start={{ x: 0, y: 0 }}
-                end={{ x: 1.6, y: 0.9 }}
+                end={{ x: 1.6, y: 0.89 }}
                 style={styles.gradient}
             />
 
             <SafeAreaView style={styles.container}>
+                <StatusBar style="auto" />
+
                 <View style={styles.navbar}>
                     <Image style={styles.icon} source={require("../assets/icons/icon-rounded.png")} />
+
+                    <View style={styles.iconShadow}></View>
 
                     <View style={styles.menu}>
                         <TouchableNativeFeedback>
@@ -25,7 +30,29 @@ export default function Home() {
                     </View>
                 </View>
 
-                <StatusBar style="auto" />
+                <View style={styles.mainbox}>
+                    <View style={styles.mainbox_text}>
+                        <Text style={{ fontSize: 19, fontWeight: "900" }}>
+                            This is a dummy text, this is not a very cool looking text and we are trying some
+                            allignment.
+                        </Text>
+
+                        <Text style={{ fontWeight: "600" }}>Some Example (22:33)</Text>
+                    </View>
+
+                    <View style={{ width: "32%" }}>
+                        <QuranSVG />
+                    </View>
+                </View>
+
+                <Text style={styles.exploreTitle}>Explore</Text>
+
+                <View style={{ display: "flex", marginTop: "3%" }}>
+                    <View style={styles.navButtons}></View>
+                    <View style={styles.navButtons}></View>
+                    <View style={styles.navButtons}></View>
+                    <View style={styles.navButtons}></View>
+                </View>
             </SafeAreaView>
         </View>
     );
@@ -43,17 +70,18 @@ const styles = StyleSheet.create({
         paddingLeft: "5%",
         paddingRight: "5%",
         width: "100%",
+        height: "100%",
     },
 
     gradient: {
         position: "absolute",
-        height: "100%",
-        width: "100%",
+        height: "105%",
+        width: "150%",
+        opacity: 0.5,
     },
 
     navbar: {
         width: "100%",
-        height: "auto",
         display: "flex",
         flexDirection: "row",
         marginTop: "5%",
@@ -63,13 +91,55 @@ const styles = StyleSheet.create({
     icon: {
         width: "15%",
         aspectRatio: 1,
-        elevation: 6,
+        zIndex: 1,
+    },
+
+    iconShadow: {
+        width: "15%",
+        aspectRatio: 1,
+        borderRadius: 15,
+        position: "relative",
+        right: "283%",
+        elevation: 11,
     },
 
     menu: {
-        height: "100%",
+        width: "15%",
         aspectRatio: 1,
         borderRadius: 18,
         overflow: "hidden",
+    },
+
+    mainbox: {
+        width: "100%",
+        height: "25%",
+        marginTop: "5%",
+        borderRadius: 15,
+        elevation: 12,
+        backgroundColor: "#F0F8FF",
+        padding: "4.5%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+
+    mainbox_text: {
+        width: "68%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+    },
+
+    exploreTitle: {
+        fontSize: 42,
+        fontWeight: "900",
+        marginTop: "5%",
+    },
+
+    navButtons: {
+        height: "12%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
 });
